ef5418c40ff6b6cd37beeb5816ae36bab928f62e
Tschouten95 <tschouten95@outlook.com>
2026-04-26 14:37:05 +0200

    refactor: update middleware and thumbnail upload logic - Rename cacheMiddleware to noCacheMiddleware for clarity - Enhance handlerUploadThumbnail to support file uploads and validation - Update bun.lock configuration

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,53 +1,66 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "node:path";
+import { randomBytes } from "node:crypto";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
+export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
+
   if (!videoId) {
     throw new BadRequestError("Invalid video ID");
   }
 
   const video = getVideo(cfg.db, videoId);
+
   if (!video) {
     throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
-
-export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
   }
 
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
+  if (video.userID != userID) {
+    throw new UserForbiddenError("Unauthorized");
+  }
+
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const MAX_UPLOAD_SIZE = 10 << 20;
 
-  return respondWithJSON(200, null);
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Thumbnail size too large");
+  }
+
+  const mediaType = file.type;
+
+  if (mediaType != "image/jpg" && mediaType != "image/png") {
+    throw new BadRequestError("Unsupported thumbnail format");
+  }
+
+  const extension = mediaType.split("/")[1]
+
+  const fileName = randomBytes(32).toString("base64url");
+  const filePath = path.join(cfg.assetsRoot, `${fileName}.${extension}`);
+
+  const data = await file.arrayBuffer();
+
+
+  await Bun.write(filePath, data);
+
+  video.thumbnailURL = `http://localhost:${cfg.port}/assets/${fileName}.${extension}`;
+
+  await updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
